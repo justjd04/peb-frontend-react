f8669d9a72c8f1e31cfd0553dc741409a7d4aeb8
Jan Dohinog <janreydohinog@gmail.com>
2022-03-12 21:38:52 +0800

    fix module

--- src/container/index.js
+++ src/container/index.js
@@ -1,6 +1,6 @@
 export { default as About } from "./About/About";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
-export { default as People } from "./Skills/People";
+export { default as People } from "./People/People";
 export { default as Testimonial } from "./Testimonial/Testimonial";
 export { default as Prc } from "./Prc/Prc";
